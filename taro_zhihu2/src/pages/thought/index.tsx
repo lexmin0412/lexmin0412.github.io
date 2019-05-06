9e3d2daea55e2235873523224d3fbd5e07d98908
cathe-zhang <zhangle_media@hotmail.com>
2019-05-06 18:03:44 +0800

    chore: 移动taro_zhihu文件夹

---
+++ taro_zhihu2/src/pages/thought/index.tsx
@@ -0,0 +1,28 @@
+import Taro , { Component } from '@tarojs/taro';
+import { View, Text , Button} from '@tarojs/components';
+
+class Index extends Component {
+
+   config = {
+       navigationBarTitleText: '想法'
+  }
+
+  state={}
+
+  componentWillMount () {}
+  componentDidMount () {} 
+  componentWillReceiveProps (nextProps,nextContext) {} 
+  componentWillUnmount () {} 
+  componentDidShow () {} 
+  componentDidHide () {} 
+  componentDidCatchError () {} 
+  componentDidNotFound () {} 
+  render() {
+    return (
+      <View>
+        想法
+      </View>
+    );
+  }
+}
+export default Index;
