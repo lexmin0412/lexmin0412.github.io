72cf61657d47e815102eb708395632e07a82d95e
cathe-zhang <zhangle_media@hotmail.com>
2019-05-14 11:14:50 +0800

    refactor: 迁移notes仓库到io主页

--- notes/ES6+/demos/demo_03_fromEntries.js
+++
@@ -1,6 +0,0 @@
-const entries = new Map([
-  ['apple', 'orange'],
-  ['grapes', 'peach']
-])
-const obj = Object.fromEntries(entries);  // 暂不支持fromEntries方法
-console.log(obj)
--- notes/repos/antd-m.md
+++
@@ -1,3 +0,0 @@
-# antd-m
-
-- picker的value属性是一个数组，而不是对象或字符串，因为picker是多层选择时，value数组的长度就是多少。
